2cc37e5375508843d0a8f7e74c721f97bb5e58b6
Jason Filsinger <jason.filsinger@gmail.com>
2011-12-23 04:00:41 -0800

    working emacs config

---
+++ common-init.el
@@ -0,0 +1,199 @@
+;; ================================================
+;; Load Paths
+;; ================================================
+(defvar emacs-plugins-path (concat emacs-sync-path "/plugins/"))
+(defvar emacs-submodules-path (concat emacs-sync-path "/submodules/"))
+(defvar emacs-snippets-path (concat emacs-sync-path "/custom/snippets/"))
+; add various load paths
+(add-to-list 'load-path emacs-sync-path)
+(add-to-list 'load-path emacs-plugins-path)
+(add-to-list 'load-path emacs-submodules-path)
+;; ================================================
+
+;; ================================================
+;; Load auto-complete and YAsnippets before we load the SlantSix utils.
+;; ================================================
+(defvar emacs-auto-complete-path (concat emacs-submodules-path "auto-complete/"))
+(add-to-list 'load-path emacs-auto-complete-path)
+(require 'auto-complete-config)
+
+
+;; ================================================
+;; SlantSix utils
+;; ================================================
+(if (or (eq system-type 'windows-nt)
+	(eq system-type 'cygwin))
+    (let ()
+      (defvar emacs-slantsix-path (concat emacs-sync-path "/submodules/slantsix/"))
+      (add-to-list 'load-path emacs-slantsix-path)
+      (load "slantsix.el") ; load SlantSix utils
+      )
+)
+;; ================================================
+
+
+
+;; ================================================
+;; Autocomplete
+;; ================================================
+;; (eval-after-load 'auto-complete-config
+;;   '(add-to-list 'ac-dictionary-directories 
+;; 		(concat emacs-sync-path "custom/ac-dict")
+;; 		(concat emacs-auto-complete-path "dict"))
+;;     )
+(add-to-list 'ac-dictionary-directories (concat emacs-sync-path "custom/ac-dict"))
+(add-to-list 'ac-dictionary-directories (concat emacs-auto-complete-path "dict"))
+(ac-config-default)
+(define-globalized-minor-mode real-global-auto-complete-mode
+  auto-complete-mode (lambda ()
+                       (if (not (minibufferp (current-buffer)))
+                         (auto-complete-mode 1))
+                       ))
+(real-global-auto-complete-mode t)
+;; ================================================
+
+;; ================================================
+;; Usage configuration
+;; ================================================
+(setq inhibit-splash-screen t)                 ; Disable the splash screen
+(setq-default transient-mark-mode t)           ; Selection highlighting
+(setq-default truncate-lines t)                ; Disable line wrapping
+(setq inhibit-startup-echo-area-message t)     ; Disable startup message
+(setq tab-always-indent 'complete)             ; Smart tabs
+(define-key global-map [ns-drag-file] 'ns-find-file) ; OSX: Drag an drop will open a new file (not append)
+(setq ns-pop-up-frames nil)                    ; open files in current window
+(global-font-lock-mode t)                      ; activate font-lock-mode (syntax coloring)
+(column-number-mode t)                         ; Activate column-number-mode
+(setq x-select-enable-clipboard t)             ; use the system clipboard
+;; backup
+(setq backup-inhibited t)                      ; disable backup
+(setq make-backup-files nil)                   ; disable backup files
+(setq auto-save-default nil)                   ; disable auto save
+;; tabs
+;;(setq-default indent-tabs-mode nil)           ; use spaces instead of tabs for indentation
+;;(setq indent-tabs-mode nil)                   ; use spaces instead of tabs for indentation
+(setq tab-width 4)                             ; tab width
+(setq c-basic-offset 4)                        ; tab width
+(normal-erase-is-backspace-mode 1)             ; fix the delete key so that it deletes instead of backspacing
+(global-linum-mode 1)                          ; enable line numbers along the side
+(setq linum-format "  %d ")                    ; set the line number formatting
+;; ================================================
+
+
+;; ================================================
+;; Key Configurations
+;; ================================================
+(global-unset-key "\C-z")       ; disable CTRL+z
+(global-set-key "\C-z" nil)     ; disable CTRL+z
+;; ================================================
+
+
+;; ================================================
+;; Highlight-Symbol
+;; ================================================
+(require 'highlight-symbol)
+(global-set-key [(control f3)] 'highlight-symbol-at-point)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-prev)
+;; ================================================
+
+
+;; ================================================
+;; ido-mode
+;; ================================================
+(require 'ido)
+;; completeting find file using ido
+(defun my-ido-find-tag ()
+    "Find a tag using ido"
+    (interactive)
+    (tags-completion-table)
+    (let (tag-names)
+      (mapc (lambda (x)
+              (unless (integerp x)
+                (push (prin1-to-string x t) tag-names)))
+            tags-completion-table)
+      (find-tag (ido-completing-read "Tag: " tag-names))))
+(ido-mode)
+;; ================================================
+
+
+;; ================================================
+;; Lua Mode
+;; ================================================
+(add-to-list 'load-path (concat emacs-submodules-path "/lua-mode"))
+(setq auto-mode-alist (cons '("\\.lua$" . lua-mode) auto-mode-alist))
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-hook 'lua-mode-hook 'turn-on-font-lock)
+(add-hook 'lua-mode-hook 'hs-minor-mode)
+;; ================================================
+
+
+;; ================================================
+;; YASnippit
+;; ================================================
+(defvar yasnippet-path (concat  emacs-submodules-path "/yasnippet/"))
+(add-to-list 'load-path yasnippet-path)
+(require 'yasnippet)
+(setq yas/snippet-dirs 
+      (list emacs-snippets-path
+	    (concat yasnippet-path "snippets")
+	    (concat emacs-sync-path "submodules/slantsix/snippets")
+	    )
+      )
+(yas/global-mode 1)
+
+;; ================================================
+
+
+;; ================================================
+;; Clang autocomplete
+;; ================================================
+(add-to-list 'load-path (concat emacs-submodules-path "/auto-complete-clang/"))
+(require 'auto-complete-clang)
+(setq ac-auto-start nil)
+(setq ac-quick-help-delay 0.5)
+(ac-set-trigger-key "TAB")
+(define-key ac-mode-map  [(control tab)] 'auto-complete)
+(defun my-ac-config ()
+  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
+  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+  ;; (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+  (add-hook 'css-mode-hook 'ac-css-mode-setup)
+  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+  (global-auto-complete-mode t))
+(defun my-ac-cc-mode-setup ()
+  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
+(add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
+;; ac-source-gtags
+(my-ac-config)
+;; ================================================
+
+
+;; ================================================
+;; git support (EGG)
+;; ================================================
+(add-to-list 'load-path (concat emacs-submodules-path "/egg/"))
+(require 'egg)
+;; ================================================
+
+
+;; ================================================
+;; gist (gist.github.com) support
+;; ================================================
+(add-to-list 'load-path (concat emacs-submodules-path "/gist/"))
+(require 'gist)
+;; ================================================
+
+;; ================================================
+;; Special highlighting of todos in c/c++
+;; ================================================
+(add-hook 'c-mode-common-hook
+               (lambda ()
+                (font-lock-add-keywords nil
+                 '(("\\<\\(FIXME\\|TODO\\|BUG\\|HACK\\):" 1 font-lock-warning-face t)))))
+;; ================================================
+
+
+
---
+++ update-submodules.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+git pull
+git submodule update --init
+git submodule foreach git checkout master
+git submodule foreach git pull
+
