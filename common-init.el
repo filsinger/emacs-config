e8a8bb66d82586b68803f743fc734951147a306e
Jason Filsinger <jason.filsinger@gmail.com>
2011-12-23 04:38:53 -0800

    pulling in 'highlight-symbol.el' as a gid submodule

--- common-init.el
+++ common-init.el
@@ -1,11 +1,9 @@
 ;; ================================================
 ;; Load Paths
 ;; ================================================
-(defvar emacs-plugins-path (concat emacs-sync-path "/plugins/"))
 (defvar emacs-submodules-path (concat emacs-sync-path "/submodules/"))
 ; add various load paths
 (add-to-list 'load-path emacs-sync-path)
-(add-to-list 'load-path emacs-plugins-path)
 (add-to-list 'load-path emacs-submodules-path)
 ;; ================================================
 
@@ -90,6 +88,7 @@
 ;; ================================================
 ;; Highlight-Symbol
 ;; ================================================
+(add-to-list 'load-path (concat emacs-submodules-path "highlight-symbol/"))
 (require 'highlight-symbol)
 (global-set-key [(control f3)] 'highlight-symbol-at-point)
 (global-set-key [f3] 'highlight-symbol-next)
