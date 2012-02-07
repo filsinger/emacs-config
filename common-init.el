95303347d8778cbcd27e3e1e25957ff3bb970d17
Jason Filsinger <jason.filsinger@gmail.com>
2012-02-07 08:37:11 -0800

    fixing C/C++ indentation.

--- common-init.el
+++ common-init.el
@@ -198,6 +198,24 @@
                  '(("\\<\\(FIXME\\|TODO\\|BUG\\|HACK\\):" 1 font-lock-warning-face t)))))
 ;; ================================================
 
+
+;; ================================================
+;; Set tab and substatement indentation settings for c/c++
+;; ================================================
+(defun my-c-mode-common-hook ()
+  ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
+  (c-set-offset 'substatement-open 0)
+  ;; other customizations can go here
+  (setq c++-tab-always-indent t)
+  (setq c-basic-offset 4)                  ;; Default is 2
+  (setq c-indent-level 4)                  ;; Default is 2
+  (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
+  (setq tab-width 4)
+  (setq indent-tabs-mode t)  ; use spaces only if nil
+  )
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+;; ================================================
+
 ;; ================================================
 ;; Predictive Mode
 ;; ================================================
