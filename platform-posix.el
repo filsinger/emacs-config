d13be72d7a8cc2e42825247980d4682368a5a121
Jason Filsinger <jason.filsinger@gmail.com>
2012-04-22 22:07:49 -0700

    clang-flags are set for include directories on OS X

--- platform-posix.el
+++ platform-posix.el
@@ -33,3 +33,23 @@
 ))
 ;; ================================================
 
+
+;; ================================================
+;; Clang autocomplete (set the clang-flags for posix-systems
+;; ================================================
+(setq ac-clang-flags
+      (mapcar (lambda (item)(concat "-I" item))
+              (split-string
+               "
+ /usr/llvm-gcc-4.2/bin/../lib/gcc/i686-apple-darwin11/4.2.1/include
+ /usr/include/c++/4.2.1
+ /usr/include/c++/4.2.1/backward
+ /usr/local/include
+ /Applications/Xcode.app/Contents/Developer/usr/llvm-gcc-4.2/lib/gcc/i686-apple-darwin11/4.2.1/include
+ /usr/include
+ /System/Library/Frameworks
+ /Library/Frameworks
+"
+               )))
+;; ================================================
+
