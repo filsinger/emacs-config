bae64101f4a55acbcdfb6aaf778dbda531ad7f1e
Jason Filsinger <jason.filsinger@gmail.com>
2012-04-22 22:06:58 -0700

    gist.el is only loaded on emacs 23 or greater.

--- platform-posix.el
+++ platform-posix.el
@@ -18,13 +18,18 @@
 (autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
 ;; ================================================
 
+
 ;; ================================================
 ;; gist (gist.github.com) support
 ;; ================================================
-(add-to-list 'load-path (concat emacs-submodules-path "/gh/"))
-(add-to-list 'load-path (concat emacs-submodules-path "/gist/"))
-(add-to-list 'load-path (concat emacs-submodules-path "/logito/"))
-(add-to-list 'load-path (concat emacs-submodules-path "/pcache/"))
-(add-to-list 'load-path (concat emacs-submodules-path "/tabulated-list/"))
-(require 'gist)
+(when (>= emacs-major-version 23)
+(progn
+  (add-to-list 'load-path (concat emacs-submodules-path "/gh/"))
+  (add-to-list 'load-path (concat emacs-submodules-path "/gist/"))
+  (add-to-list 'load-path (concat emacs-submodules-path "/logito/"))
+  (add-to-list 'load-path (concat emacs-submodules-path "/pcache/"))
+  (when (< emacs-major-version 24) (add-to-list 'load-path (concat emacs-submodules-path "/tabulated-list/")))
+  (require 'gist)
+))
 ;; ================================================
+
