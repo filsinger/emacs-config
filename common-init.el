83d6763c5978ba42a434c5eaedddddfd07b32111
Jason Filsinger <jason.filsinger@gmail.com>
2012-05-03 16:08:38 -0700

    adding 'smarttabs' submodule

--- common-init.el
+++ common-init.el
@@ -29,6 +29,29 @@
 )
 ;; ================================================
 
+;; ================================================
+;; smart-tabs-mode (https://github.com/jcsalomon/smarttabs)
+;; ================================================
+(add-to-list 'load-path (concat emacs-submodules-path "smarttabs/"))
+(require 'smart-tabs-mode)
+;; C/C++
+(add-hook 'c-mode-hook 'smart-tabs-mode-enable)
+(smart-tabs-advice c-indent-line c-basic-offset)
+(smart-tabs-advice c-indent-region c-basic-offset)
+;; JavaScript
+(add-hook 'js2-mode-hook 'smart-tabs-mode-enable)
+(smart-tabs-advice js2-indent-line js2-basic-offset)
+;; Perl (cperl-mode)
+(add-hook 'cperl-mode-hook 'smart-tabs-mode-enable)
+(smart-tabs-advice cperl-indent-line cperl-indent-level)
+;; Python
+(add-hook 'python-mode-hook 'smart-tabs-mode-enable)
+(smart-tabs-advice python-indent-line-1 python-indent)
+;; Ruby
+(add-hook 'ruby-mode-hook 'smart-tabs-mode-enable)
+(smart-tabs-advice ruby-indent-line ruby-indent-level)
+;; ================================================
+
 
 ;; ================================================
 ;; Popup
