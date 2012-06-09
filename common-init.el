4b1d619c14e0bc90caa49b6300cd30f1aad078ab
Jason Filsinger <jason.filsinger@gmail.com>
2012-06-09 16:04:56 -0700

    tabs in ruby

--- common-init.el
+++ common-init.el
@@ -298,6 +298,19 @@
 
 
 ;; ================================================
+;; Set tab and substatement indentation settings for ruby
+;; ================================================
+(add-hook 'ruby-mode-hook (lambda ()
+  (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
+  (setq tab-width 4)
+  (setq indent-tabs-mode t)  ;; use spaces only if nil
+  (subword-mode 1)	     ;; enable subword mode
+  (which-function-mode t)    ;; enabling which-function-mode in ruby
+  ))
+;; ================================================
+
+
+;; ================================================
 ;; Predictive Mode
 ;; ================================================
 (add-to-list 'load-path (concat emacs-submodules-path "/predictive-mode/"))
