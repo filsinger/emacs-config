3a752cece36fc4e225caeed4180c7e1e5477dff8
Jason Filsinger <jfilsinger@Kusanagi.local>
2012-07-07 02:19:54 -0700

    Adding a function to generate a huge autoload file for all of the submodules and items in the custom folder.

---
+++ custom/jf-generate-autoload.el
@@ -0,0 +1,21 @@
+
+(defun jf-sync-subpath (path)
+  ""
+  (expand-file-name (concat emacs-sync-path "/" path "/")))
+
+;;;###autoload
+(defun jf-generate-autoload-files ()
+  ""
+  (interactive)
+  (let ((generated-autoload-file (expand-file-name (concat emacs-autoloads-path "my-super-autoload.el"))))
+
+	;; add everything in the custom directory
+	(update-directory-autoloads (jf-sync-subpath "custom"))
+
+	;; add all of the submodules
+	(dolist (submodule (directory-files emacs-submodules-path t "\\w+"))
+	  (when (file-directory-p submodule)
+		(update-directory-autoloads submodule)))
+
+	(byte-recompile-directory emacs-autoloads-path)
+	))
