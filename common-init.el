c04b1bafa157af4d4101b47e1e0e8b7bb8f2f0ff
Jason Filsinger <jfilsinger@Kusanagi.local>
2012-07-07 00:03:04 -0700

    moved 'my-ido-find-tag' to an external file and autoload it now.

--- common-init.el
+++ common-init.el
@@ -211,17 +211,7 @@
 ;; ================================================
 ;; ido-mode
 ;; ================================================
-;; completing find file using ido
-(defun my-ido-find-tag ()
-    "Find a tag using ido"
-    (interactive)
-    (tags-completion-table)
-    (let (tag-names)
-      (mapc (lambda (x)
-              (unless (integerp x)
-                (push (prin1-to-string x t) tag-names)))
-            tags-completion-table)
-      (find-tag (ido-completing-read "Tag: " tag-names))))
+(autoload 'my-ido-find-tag "my-ido-find-tag" "Completing find file using ido" t)
 (ido-mode)
 ;; ================================================
 
---
+++ custom/my-ido-find-tag.el
@@ -0,0 +1,11 @@
+;; completing find file using ido
+(defun my-ido-find-tag ()
+    "Find a tag using ido"
+    (interactive)
+    (tags-completion-table)
+    (let (tag-names)
+      (mapc (lambda (x)
+              (unless (integerp x)
+                (push (prin1-to-string x t) tag-names)))
+            tags-completion-table)
+      (find-tag (ido-completing-read "Tag: " tag-names))))
