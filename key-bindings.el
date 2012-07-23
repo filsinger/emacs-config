eaeafdf266f172df79c75387963a076721751e3a
Jason Filsinger <jason.filsinger@gmail.com>
2012-07-22 21:33:22 -0700

    cleaning up 'key-bindings.el'

--- key-bindings.el
+++ key-bindings.el
@@ -2,34 +2,32 @@
 ;; Key Configurations
 ;; ================================================
 
-(global-unset-key "\C-z")                     ; disable CTRL+z
+;; Unset keys
 (global-set-key "\C-z" nil)                   ; disable CTRL+z
-(when (eq window-system nil) (global-set-key "\C-d" 'backward-delete-char)) ; make sure backspace works the way I like in the OSX terminal
-(global-set-key "\M-g-g" 'goto-line)          ; bind M-g-g to goto-line
-(global-set-key "\C-c\C-c" 'comment-or-uncomment-region)          ; bind C-u to comment toggle
-(global-set-key "\C-a" `align-entire)       ; bind C-a to align-entire
-(global-set-key "\M-s" `sort-lines)         ; bind M-s to sort-lines
-(global-set-key (kbd "C-S-s") 'tags-apropos) ; tags apropos
+(global-unset-key "\C-z")                     ; disable CTRL+z
 (global-unset-key (kbd "C-."))
 (global-unset-key (kbd "C-,"))
+
+
+(when (eq window-system nil) (global-set-key "\C-d" 'backward-delete-char)) ; make sure backspace works the way I like in the OSX terminal
+(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region) ; bind C-u to comment toggle
+(global-set-key (kbd "C-c a") 'align-entire) ; bind C-a to align-entire
+(global-set-key (kbd "M-s") 'sort-lines)	 ; bind m-s to sort-lines
+(global-set-key (kbd "C-S-s") 'tags-apropos) ; tags apropos
 (global-set-key (kbd "C-.") 'next-multiframe-window) ; use C-. to move to the next window
 (global-set-key (kbd "C-,") 'previous-multiframe-window) ; use C-, to move to the previous window
-
 
 ;; hilight-symbol
 (global-set-key (kbd "C-<f3>") 'highlight-symbol-at-point)
 (global-set-key (kbd "<f3>")   'highlight-symbol-next)
 (global-set-key (kbd "S-<f3>") 'highlight-symbol-prev)
-(global-set-key (kbd "M-<f3>") 'highlight-symbol-prev)
+(global-set-key (kbd "M-<f3>") 'highlight-symbol-perv)
 
-;; ace-jump-mode
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+;; spelling
+(global-set-key (kbd "<f8>") 'ispell-word) ; spell-check the current word
 
 ;; helm
 (global-set-key (kbd "C-x C-a") 'helm-mini)
-
-;; expand-region
-(global-set-key (kbd "C-@") 'er/expand-region)
 
 ;; mark-multiple
 (global-set-key (kbd "C-x r t") 'inline-string-rectangle)
@@ -37,16 +35,43 @@
 (global-set-key (kbd "C->") 'mark-next-like-this)
 (global-set-key (kbd "C-M-m") 'mark-more-like-this) ; like the other two, but takes an argument (negative is previous)
 
-;; eval-and-replace
-(global-set-key (kbd "C-c e") 'fc-eval-and-replace)
-
-;; go to companion file
-(global-set-key (kbd "C-c o") 'ff-get-other-file)
+;; region
+(global-set-key (kbd "C-c e") 'eval-and-replace)       ; evaluate and replace the region
+(global-set-key (kbd "C-@") 'er/expand-region)         ; expand-region
 
 ;; set the desired behavior for home and end keys
-(global-set-key (kbd "<home>") 'beginning-of-line)
-(global-set-key (kbd "<end>") 'end-of-line)
-(global-set-key (kbd "C-<home>") 'beginning-of-buffer)
-(global-set-key (kbd "C-<end>") 'end-of-buffer)
+(global-set-key "\M-g-g" 'goto-line)                   ; move cursor to a specific line
+(global-set-key (kbd "<home>") 'beginning-of-line)     ; move cursor to the begenning of the line
+(global-set-key (kbd "<end>") 'end-of-line)            ; move cursor to the end of the line
+(global-set-key (kbd "C-<home>") 'beginning-of-buffer) ; move cursor to the begenning of the buffer
+(global-set-key (kbd "C-<end>") 'end-of-buffer)		   ; move cursor to the end of the buffer
+(global-set-key (kbd "C-c SPC") 'ace-jump-mode)		   ; move cursor via ace-jump-mode
+(global-set-key (kbd "<C-M-down>") 'move-line-down)    ; move current line up
+(global-set-key (kbd "<C-M-up>") 'move-line-up)	       ; move current line down
+
+;; transpose key bindings
+(global-unset-key (kbd "M-t"))      ; unbind the default transpose-words keybinding
+(global-set-key (kbd "M-t c") 'transpose-chars)
+(global-set-key (kbd "M-t l") 'transpose-lines)
+(global-set-key (kbd "M-t p") 'transpose-paragraphs)
+(global-set-key (kbd "M-t s") 'transpose-sexps)
+(global-set-key (kbd "M-t w") 'transpose-words)
+
+;; file finding
+(global-set-key (kbd "C-x C-f") 'ido-find-file)              ; use ido to find file and open in the current window.
+(global-set-key (kbd "C-x M-f") 'ido-find-file-other-window) ; use ido to find file and open in the other window.
+(global-set-key (kbd "C-c r") 'revert-buffer)                ; revert the current buffer.
+(global-set-key (kbd "C-x C-b") 'ibuffer-other-window)       ; use ibuffer instead of the default buffer-menu.
+(global-set-key (kbd "C-c o") 'ff-get-other-file)            ; find compainion file.
+
+;; mode keymap rebinding
+(eval-after-load "ibuffer" 				; rebind ibuffer keys
+  '(progn
+	 (define-key ibuffer-mode-map (kbd "C-x C-f") 'ibuffer-ido-find-file)))
+(eval-after-load "flyspell"				; rebind flyspell keys
+  '(progn
+	 (define-key flyspell-mode-map (kbd "C-,") nil)
+	 (define-key flyspell-mode-map (kbd "C-.") nil)
+	 (define-key flyspell-mode-map (kbd "<f7>") 'flyspell-goto-next-error)))
 
 (provide 'key-bindings)
