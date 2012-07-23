25bc064cfaf16126c54edc5295215866f68c9715
Jason Filsinger <jason.filsinger@gmail.com>
2012-07-22 21:21:24 -0700

    adding move-line.el

---
+++ custom/move-line.el
@@ -0,0 +1,20 @@
+;; This came from https://github.com/magnars/.emacs.d/blob/master/defuns/editing-defuns.el
+
+;;;###autoload
+(defun move-line-down ()
+  (interactive)
+  (let ((col (current-column)))
+    (save-excursion
+      (next-line)
+      (transpose-lines 1))
+    (next-line)
+    (move-to-column col)))
+
+;;;###autoload
+(defun move-line-up ()
+  (interactive)
+  (let ((col (current-column)))
+    (save-excursion
+      (next-line)
+      (transpose-lines -1))
+    (move-to-column col)))
