43fdbd120dae5c3e260198b25014635953193be6
Jason Filsinger <jason.filsinger@gmail.com>
2012-10-13 17:08:49 -0700

    adding colors for 'magit-diff'

--- common-init.el
+++ common-init.el
@@ -378,6 +378,18 @@
 
 
 ;; ================================================
+;; magit settings
+;; ================================================
+(eval-after-load "magit"
+  '(progn
+     (set-face-foreground 'magit-diff-add "green3")
+     (set-face-foreground 'magit-diff-del "red3")
+     (when (not window-system)
+       (set-face-background 'magit-item-highlight "black"))))
+;; ================================================
+
+
+;; ================================================
 ;; Load Key Bindings
 ;; ================================================
 (load "key-bindings.el")
