3862a8a876c691709ec19b6ffb2f2c654bf51cdf
Jason Filsinger <jason.filsinger@gmail.com>
2013-10-09 18:21:22 -0700

    reducing the max number of items saved in recentf

--- common-init.el
+++ common-init.el
@@ -193,7 +193,7 @@
 (ido-mode 1)							  ; enable ido mode
 
 ;; recentf-mode
-(setq recentf-max-saved-items 200
+(setq recentf-max-saved-items 10
       recentf-max-menu-items 15)
 (recentf-mode +1)
 
