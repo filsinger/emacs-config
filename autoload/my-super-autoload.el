d70054f7c1842c283c7718095bc0b006ae55673b
Jason Filsinger <jason.filsinger@gmail.com>
2014-02-26 23:41:59 -0800

    Adding 'git-modes' submodule

--- autoload/my-super-autoload.el
+++ autoload/my-super-autoload.el
@@ -1484,6 +1484,86 @@ Displays a list of all of the current user's gists in a new buffer.
 
 ;;;***
 
+;;;### (autoloads (git-commit-mode) "../submodules/git-modes/git-commit-mode"
+;;;;;;  "../submodules/git-modes/git-commit-mode.el" (21262 60392
+;;;;;;  0 0))
+;;; Generated autoloads from ../submodules/git-modes/git-commit-mode.el
+
+(autoload 'git-commit-mode "../submodules/git-modes/git-commit-mode" "\
+Major mode for editing git commit messages.
+
+This mode helps with editing git commit messages both by
+providing commands to do common tasks, and by highlighting the
+basic structure of and errors in git commit messages.
+
+\(fn)" t nil)
+
+(dolist (pattern '("/COMMIT_EDITMSG\\'" "/NOTES_EDITMSG\\'" "/MERGE_MSG\\'" "/TAG_EDITMSG\\'" "/PULLREQ_EDITMSG\\'")) (add-to-list 'auto-mode-alist (cons pattern 'git-commit-mode)))
+
+;;;***
+
+;;;### (autoloads (git-rebase-mode) "../submodules/git-modes/git-rebase-mode"
+;;;;;;  "../submodules/git-modes/git-rebase-mode.el" (21262 60392
+;;;;;;  0 0))
+;;; Generated autoloads from ../submodules/git-modes/git-rebase-mode.el
+
+(autoload 'git-rebase-mode "../submodules/git-modes/git-rebase-mode" "\
+Major mode for editing of a Git rebase file.
+
+Rebase files are generated when you run 'git rebase -i' or run
+`magit-interactive-rebase'.  They describe how Git should perform
+the rebase.  See the documentation for git-rebase (e.g., by
+running 'man git-rebase' at the command line) for details.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("/git-rebase-todo\\'" . git-rebase-mode))
+
+;;;***
+
+;;;### (autoloads (gitattributes-mode) "../submodules/git-modes/gitattributes-mode"
+;;;;;;  "../submodules/git-modes/gitattributes-mode.el" (21262 60392
+;;;;;;  0 0))
+;;; Generated autoloads from ../submodules/git-modes/gitattributes-mode.el
+
+(autoload 'gitattributes-mode "../submodules/git-modes/gitattributes-mode" "\
+A major mode for editing .gitattributes files.
+\\{gitattributes-mode-map}
+
+\(fn)" t nil)
+
+(dolist (pattern '("/\\.gitattributes\\'" "/\\.git/info/attributes\\'" "/git/attributes\\'")) (add-to-list 'auto-mode-alist (cons pattern #'gitattributes-mode)))
+
+;;;***
+
+;;;### (autoloads (gitconfig-mode) "../submodules/git-modes/gitconfig-mode"
+;;;;;;  "../submodules/git-modes/gitconfig-mode.el" (21262 60392
+;;;;;;  0 0))
+;;; Generated autoloads from ../submodules/git-modes/gitconfig-mode.el
+
+(autoload 'gitconfig-mode "../submodules/git-modes/gitconfig-mode" "\
+A major mode for editing .gitconfig files.
+
+\(fn)" t nil)
+
+(dolist (pattern '("/\\.gitconfig\\'" "/\\.git/config\\'" "/git/config\\'" "/\\.gitmodules\\'")) (add-to-list 'auto-mode-alist (cons pattern 'gitconfig-mode)))
+
+;;;***
+
+;;;### (autoloads (gitignore-mode) "../submodules/git-modes/gitignore-mode"
+;;;;;;  "../submodules/git-modes/gitignore-mode.el" (21262 60392
+;;;;;;  0 0))
+;;; Generated autoloads from ../submodules/git-modes/gitignore-mode.el
+
+(autoload 'gitignore-mode "../submodules/git-modes/gitignore-mode" "\
+A major mode for editing .gitignore files.
+
+\(fn)" t nil)
+
+(dolist (pattern (list "/\\.gitignore\\'" "/\\.git/info/exclude\\'" "/git/ignore\\'")) (add-to-list 'auto-mode-alist (cons pattern 'gitignore-mode)))
+
+;;;***
+
 ;;;### (autoloads (glsl-mode) "../submodules/glsl-mode/glsl-mode"
 ;;;;;;  "../submodules/glsl-mode/glsl-mode.el" (20955 14013 0 0))
 ;;; Generated autoloads from ../submodules/glsl-mode/glsl-mode.el
@@ -5783,7 +5863,7 @@ accept it or skip it.
 ;;;;;;  "../submodules/w3m/w3m-util.el" "../submodules/w3m/w3m-xmas.el"
 ;;;;;;  "../submodules/w3m/w3mhack.el" "../submodules/web-mode/wfs-mode.el"
 ;;;;;;  "../submodules/yasnippet/yasnippet-debug.el" "../submodules/yasnippet/yasnippet-tests.el")
-;;;;;;  (21198 47495 379592 0))
+;;;;;;  (21262 60401 298150 0))
 
 ;;;***
 
