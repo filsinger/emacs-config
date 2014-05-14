a6e6436f15fdc78f2eb045d7873d7a4b646076ec
Jason Filsinger <jason.filsinger@gmail.com>
2014-05-14 01:09:29 -0700

    fixing indentation in key-bindings.el

--- key-bindings.el
+++ key-bindings.el
@@ -8,12 +8,11 @@
 (global-unset-key (kbd "C-."))
 (global-unset-key (kbd "C-,"))
 
-
 (when (eq window-system nil) (global-set-key "\C-d" 'backward-delete-char)) ; make sure backspace works the way I like in the OSX terminal
 (global-set-key (kbd "S-<return>") 'smart-open-line) ; bind S-return to create a new indented line below the current line
 (global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region) ; bind C-u to comment toggle
 (global-set-key (kbd "C-c a") 'align-entire) ; bind C-a to align-entire
-(global-set-key (kbd "M-s") 'sort-lines)	 ; bind m-s to sort-lines
+(global-set-key (kbd "M-s") 'sort-lines)     ; bind m-s to sort-lines
 (global-set-key (kbd "C-S-s") 'tags-apropos) ; tags apropos
 (global-set-key (kbd "C-.") 'next-multiframe-window) ; use C-. to move to the next window
 (global-set-key (kbd "C-,") 'previous-multiframe-window) ; use C-, to move to the previous window
@@ -58,11 +57,11 @@
 (global-set-key (kbd "<home>") 'beginning-of-line)     ; move cursor to the begenning of the line
 (global-set-key (kbd "<end>") 'end-of-line)            ; move cursor to the end of the line
 (global-set-key (kbd "C-<home>") 'beginning-of-buffer) ; move cursor to the begenning of the buffer
-(global-set-key (kbd "C-<end>") 'end-of-buffer)		   ; move cursor to the end of the buffer
+(global-set-key (kbd "C-<end>") 'end-of-buffer)        ; move cursor to the end of the buffer
 (global-set-key (kbd "<C-M-down>") 'move-line-down)    ; move current line up
-(global-set-key (kbd "<C-M-up>") 'move-line-up)	       ; move current line down
+(global-set-key (kbd "<C-M-up>") 'move-line-up)        ; move current line down
 (global-set-key (kbd "C-;") 'ido-imenu)
-(global-set-key (kbd "C-c SPC") 'ace-jump-mode)		   ; move cursor via ace-jump-mode
+(global-set-key (kbd "C-c SPC") 'ace-jump-mode)        ; move cursor via ace-jump-mode
 (global-set-key (kbd "C-<left>") 'backward-word)       ; move backward word
 (global-set-key (kbd "C-<right>") 'forward-word)       ; move forward word
 (global-set-key (kbd "C-S-<left>") 'backward-sexp)     ; move backward sexp
@@ -84,12 +83,12 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer-other-window)       ; use ibuffer instead of the default buffer-menu.
 (global-set-key (kbd "C-c o") 'ff-get-other-file)            ; find compainion file.
 (add-hook 'dired-mode-hook
-		  (lambda ()
-			(put 'dired-find-alternate-file 'disabled nil)
-			(define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file ".."))) ; dired will reuse the same buffer when using ^ to navigate to the parent directory
-			(define-key dired-mode-map (kbd "<return>") (lambda () (interactive) (if (file-directory-p (dired-file-name-at-point)) (dired-find-alternate-file) (dired-find-file)))) ; make <return> open a new directory in the current buffer
-			(define-key dired-mode-map (kbd "C-<return>") (lambda () (interactive) (dired-find-file))) ; C-<return> will always use dired-find-file
-			))
+          (lambda ()
+            (put 'dired-find-alternate-file 'disabled nil)
+            (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file ".."))) ; dired will reuse the same buffer when using ^ to navigate to the parent directory
+            (define-key dired-mode-map (kbd "<return>") (lambda () (interactive) (if (file-directory-p (dired-file-name-at-point)) (dired-find-alternate-file) (dired-find-file)))) ; make <return> open a new directory in the current buffer
+            (define-key dired-mode-map (kbd "C-<return>") (lambda () (interactive) (dired-find-file))) ; C-<return> will always use dired-find-file
+            ))
 
 ;; smex (like ido for M-x)
 (global-set-key (kbd "M-x") 'smex)
@@ -102,35 +101,35 @@
 (global-set-key (kbd "C-'") 'yas-insert-snippet)
 
 ;; mode keymap rebinding
-(eval-after-load "ibuffer" 				; rebind ibuffer keys
+(eval-after-load "ibuffer"              ; rebind ibuffer keys
   '(progn
-	 (define-key ibuffer-mode-map (kbd "C-x C-f") 'ibuffer-ido-find-file)))
+     (define-key ibuffer-mode-map (kbd "C-x C-f") 'ibuffer-ido-find-file)))
 (eval-after-load "flyspell"				; rebind flyspell keys
   '(progn
-	 (define-key flyspell-mode-map (kbd "C-,") nil)
-	 (define-key flyspell-mode-map (kbd "C-.") nil)
-	 (define-key flyspell-mode-map (kbd "<f7>") 'flyspell-goto-next-error)))
+     (define-key flyspell-mode-map (kbd "C-,") nil)
+     (define-key flyspell-mode-map (kbd "C-.") nil)
+     (define-key flyspell-mode-map (kbd "<f7>") 'flyspell-goto-next-error)))
 (eval-after-load "org"					; rebind org keys (i use C-, and C-. to switch windows)
   '(progn
-	 (define-key org-mode-map (kbd "C-,") nil)
-	 (define-key org-mode-map (kbd "C-.") nil)))
+     (define-key org-mode-map (kbd "C-,") nil)
+     (define-key org-mode-map (kbd "C-.") nil)))
 (eval-after-load "zencoding-mode"
   '(progn
-	 (define-key zencoding-mode-keymap (kbd "<C-return>") 'zencoding-expand-line)
-	 ))
+     (define-key zencoding-mode-keymap (kbd "<C-return>") 'zencoding-expand-line)
+     ))
 (eval-after-load "w3m"
   '(progn
-	 (define-key w3m-mode-map (kbd "C-M-<down>") 'w3m-next-anchor)
-	 (define-key w3m-mode-map (kbd "C-M-<up>") 'w3m-previous-anchor)
-	 (define-key w3m-mode-map (kbd "C-M-<left>") 'w3m-view-previous-page)
-	 (define-key w3m-mode-map (kbd "C-M-<right>") 'w3m-view-this-url)
+     (define-key w3m-mode-map (kbd "C-M-<down>") 'w3m-next-anchor)
+     (define-key w3m-mode-map (kbd "C-M-<up>") 'w3m-previous-anchor)
+     (define-key w3m-mode-map (kbd "C-M-<left>") 'w3m-view-previous-page)
+     (define-key w3m-mode-map (kbd "C-M-<right>") 'w3m-view-this-url)
 
-	 (define-key w3m-mode-map (kbd "<down>") nil)
-	 (define-key w3m-mode-map (kbd "<up>") nil)
-	 (define-key w3m-mode-map (kbd "<left>") nil)
-	 (define-key w3m-mode-map (kbd "<right>") nil)
+     (define-key w3m-mode-map (kbd "<down>") nil)
+     (define-key w3m-mode-map (kbd "<up>") nil)
+     (define-key w3m-mode-map (kbd "<left>") nil)
+     (define-key w3m-mode-map (kbd "<right>") nil)
 
-	 )
+     )
   )
 
 ;; compiling
