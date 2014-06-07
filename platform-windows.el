b70bc027aa6c7a41d136652f670a5b1585acc62f
Jason Filsinger <jason.filsinger@gmail.com>
2014-06-07 01:12:56 -0700

    adding clipboard support within cygwin

--- platform-windows.el
+++ platform-windows.el
@@ -19,6 +19,20 @@
 	))
 
 ;; ================================================
+;; cygwin clipboard support
+(when (and (not (display-graphic-p)) (eq system-type 'cygwin))
 
+  (defun paste-from-cygwin ()
+    (with-temp-buffer
+      (insert-file-contents "/dev/clipboard")
+      (buffer-string)))
+
+  (defun cut-to-cygwin (text &optional push)
+    (with-temp-file "/dev/clipboard"
+      (insert text)))
+
+  (setq interprogram-cut-function 'cut-to-cygwin)
+  (setq interprogram-paste-function 'paste-from-cygwin)
+  )
 
 ) ;; end if test for windows system
