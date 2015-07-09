90a5687ef2162ffa1d610b3a4ebcc590d1a7e550
Jason Filsinger <jason.filsinger@gmail.com>
2015-07-09 11:47:01 -0700

    adding kill-org-filename-function-line function

--- custom/kill-filename-function-line.el
+++ custom/kill-filename-function-line.el
@@ -4,3 +4,12 @@
   (kill-new (format "%s:%s: %s" (file-name-nondirectory (buffer-file-name))
                    (count-lines (point-min) (point))
                                 (which-function))) )
+
+;;;###autoload
+(defun kill-org-filename-function-line ()
+  (interactive)
+  (kill-new (format "[[file:%s::%s][%s:%s]]: %s"
+                    (buffer-file-name) (count-lines (point-min) (point))
+                    (file-name-nondirectory (buffer-file-name))
+                   (count-lines (point-min) (point))
+                                (which-function))) )
