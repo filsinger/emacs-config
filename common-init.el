bd0b69c3985af2b356552cabbb18821c8b1b0b37
Jason Filsinger <jason.filsinger@gmail.com>
2016-09-19 15:23:55 -0700

    prevent killing the scratch buffer

--- common-init.el
+++ common-init.el
@@ -621,6 +621,16 @@
 ;; ================================================
 
 ;; ================================================
+;; bury *scratch* buffer instead of kill it
+;; ================================================
+(defadvice kill-buffer (around kill-buffer-around-advice activate)
+  (let ((buffer-to-kill (ad-get-arg 0)))
+    (if (equal buffer-to-kill "*scratch*")
+        (bury-buffer)
+      ad-do-it)))
+;; ================================================
+
+;; ================================================
 ;; Load Key Bindings
 ;; ================================================
 (load "key-bindings.el")
