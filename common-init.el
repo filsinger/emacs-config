75a1f370137d4baa8340e971dac2d5cc96b91d79
Jason Filsinger <jason@filsinger.me>
2023-08-25 16:43:56 -0700

    With ediff, pressing d will copy both A and B to C

--- common-init.el
+++ common-init.el
@@ -641,6 +641,18 @@
             ))
 ;; ================================================
 
+;; ================================================
+;; ediff
+;; ================================================
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+;; ================================================
 
 ;; ================================================
 ;; change the default locations for smex and ido saved files
