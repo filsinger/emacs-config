4f1763ee74a6a9ffe02e04b912477396a31183a1
Jason Filsinger <jason@filsinger.me>
2023-08-25 16:06:15 -0700

    Adjust lsp-mode performance

--- common-init.el
+++ common-init.el
@@ -245,6 +245,9 @@
 ;; ================================================
 (require 'lsp-mode)
 (add-hook 'c-mode-hook 'lsp)
+;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
 ;; ================================================
 
 ;; ================================================
