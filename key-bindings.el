d3a9ed473b464bce7835315df28cc6451ea749f9
Jason Filsinger <jason@filsinger.me>
2024-10-17 23:07:26 -0700

    Fixing lambda on scroll wheel bindings

--- key-bindings.el
+++ key-bindings.el
@@ -152,10 +152,10 @@
   ;; enable mouse support
   (require 'mouse)
   (xterm-mouse-mode t)
-  (global-set-key [mouse-4] '(lambda ()
+  (global-set-key [mouse-4] (lambda ()
                               (interactive)
                               (scroll-down 1)))
-  (global-set-key [mouse-5] '(lambda ()
+  (global-set-key [mouse-5] (lambda ()
                               (interactive)
                               (scroll-up 1)))
   (defun track-mouse (e))
