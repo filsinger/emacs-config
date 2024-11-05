a2e3b631fa640f4e9418dbddbac24f2e2d8bc2d6
Jason Filsinger <jason@filsinger.me>
2024-11-04 18:34:02 -0800

    Windows - Compilation error regex

--- platform-windows.el
+++ platform-windows.el
@@ -41,4 +41,25 @@
   (setq interprogram-paste-function 'paste-from-cygwin)
   )
 
+;; ================================================
+;; Compilation error regex
+;; From: https://stackoverflow.com/questions/4556368/compiling-c-with-emacs-on-windows-system/4589933#4589933
+(mapcar
+ (lambda (x)
+   (add-to-list 'compilation-error-regexp-alist-alist x))
+
+ (list
+  ;; Microsoft C/C++:
+  ;;  keyboard.c(537) : warning C4005: 'min' : macro redefinition
+  ;;  d:\tmp\test.c(23) : error C2143: syntax error : missing ';' before 'if'
+  ;;  .\cppcli1.cpp(36): error C2059: syntax error : 'public'
+  ;;  e:\projects\myce40\tok.h(85) : error C2236: unexpected 'class' '$S1'
+  ;;  myc.cpp(14) : error C3149: 'class System::String' : illegal use of managed type 'String'; did you forget a '*'?
+  ;;   ("\\(\\([a-zA-Z]:\\)?[^:(\t\n]+\\)(\\([0-9]+\\)) ?\: \\(error\\|warning\\) C[0-9]+:" 1 3)
+  '(msvc "^[ \t]*\\([A-Za-z0-9\\.][^(]*\\.\\(cpp\\|c\\|h\\)\\)(\\([0-9]+\\)) *: +\\(error\\|fatal error\\|warning\\) C[0-9]+:" 1 3)
+  ))
+
+(setq compilation-error-regexp-alist
+      (mapcar 'car compilation-error-regexp-alist-alist))
+
 ) ;; end if test for windows system
