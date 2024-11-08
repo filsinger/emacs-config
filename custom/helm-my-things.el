6777a73c5227ee00800f5d9fcbed02a75022f086
Jason Filsinger <jason@filsinger.me>
2024-11-08 01:43:23 -0800

    Replace helm-source-everything with helm-source-locate

--- custom/helm-my-things.el
+++ custom/helm-my-things.el
@@ -1,8 +1,8 @@
 (require 'helm)
 (require 'helm-buffers)
-(require 'helm-everything)
 (require 'helm-files)
 (require 'helm-imenu)
+(require 'helm-locate)
 
 ;;;###autoload
 (defun helm-mine ()
@@ -11,9 +11,8 @@
     (helm-other-buffer
      '(helm-source-buffers-list
        helm-source-recentf
-       helm-source-everything
+       helm-source-locate
        helm-source-imenu
-       ;;helm-source-buffer-not-found
        )
      "*helm mine"
      )
