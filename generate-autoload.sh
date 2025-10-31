73ccda18fc71e6de41a12a3ac5c393b5cd311541
Jason Filsinger <jason@filsinger.me>
2025-10-30 22:43:51 -0700

    Update generate-autoload.sh script

--- generate-autoload.sh
+++ generate-autoload.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # delete all previously compiled elc files
-# find . -name \*.elc -type f -delete
+# find lisp/ -name \*.elc -type f -delete
 
 # generate autoload files
-emacs -batch -l "$(dirname $(realpath $0))/init.el" --eval="(jf-generate-autoload-files)"
+emacs -batch -l "$(dirname $(realpath $0))/init.el" -f jf-generate-autoload-files
 
 # byte compile el files
 # emacs -batch -l ~/.emacs --eval="(byte-recompile-directory emacs-sync-path 0)"
